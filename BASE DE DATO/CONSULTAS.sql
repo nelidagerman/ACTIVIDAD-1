29438ab94a9d8c269179b93637fe6e210bc0bb35
nelidagerman <36268164+nelidagerman@users.noreply.github.com>
2018-02-12 18:28:10 -0500

    Add files via upload

--- BASE DE DATO/CONSULTAS.sql
+++ BASE DE DATO/CONSULTAS.sql
@@ -2,3 +2,19 @@ insert into usuario (correo, contraseña) value ('heiderzapa78@gmail.com','heider
 //dato entrada: correo y contraseña//
 //dato salida: registro de un nuevo usuario//
 //esta consulta ingresa a la base de dato un nuevo usuario//
+
+
+select nombre, direcion restaurante where direccion= "k20a #12-b43"
+//dato entrada: nombre y direccion//
+//dato salida: nombre y la direccion del restaurante que esta en k20a #12-b43//
+//esta consulta muestra el los datos especificos de una determinada tabla//
+
+update cliente set apellido2= "Contreras" where nombre1= "Nelida" and apellido2= "Martinez"
+//dato entrada: apellido2 tabla cliente//
+//dato salida: nombre1 y apellido2 de la table cliente//
+//esta consulta cambia un dato de una tabla de la base de datos//
+
+delete *from restauarntes where nombre= "Yogi"
+//dato entrada: nombre= Yogi//
+//dato salida:registro borrado//
+//eata consulta borra un dato de una tabla de la base de datos// 
